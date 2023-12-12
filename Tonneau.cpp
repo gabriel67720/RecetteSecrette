8677c86f30b6e112a54e9294368780cea16f6db3
Gabriel CABRAL DA SILVA <gcabraldasilva1@numerique-educatif.fr>
2023-12-12 20:27:25 +0100

    Ajoutez des fichiers projet.

---
+++ Tonneau.cpp
@@ -0,0 +1,33 @@
+#include "Tonneau.h"
+
+void Tonneau::remplir()
+{
+	contenu = contenance;
+}
+
+void Tonneau::vider()
+{
+	contenu = 0;
+}
+
+void Tonneau::transferer(Tonneau dest)
+{
+	
+	if ()
+	;
+}
+
+int Tonneau::getContenu()
+{
+	return contenu;
+}
+
+int Tonneau::getContenant()
+{
+	return contenance;
+}
+
+void Tonneau::caparestante()
+{
+	contenance - contenu;
+}
---
+++ Tonneau.h
@@ -0,0 +1,19 @@
+#pragma once
+class Tonneau
+{
+private:
+	int contenance, contenu;
+
+public:
+	void remplir();
+	void vider();
+	void transferer(Tonneau dest);
+	void afftonneau();
+	int getContenu();
+	int getContenant();
+	Tonneau(int capacite);
+	void caparestante();
+
+
+};
+
